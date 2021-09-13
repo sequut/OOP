5027a357f0d227c489aec7d84108af3a570c4f79
Andrew <andrewkostin255@ya.ru>
2021-09-13 17:36:24 +0700

    updated tests

--- Heapsort/src/test/java/Heapsorttest.java
+++ Heapsort/src/test/java/Heapsorttest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Heapsorttest {
 
     @Test
-    public void test_1() {
+    public void check_positive_num() {
         int[] input = new int[]{5, 4, 3, 2, 1};
         int[] answer = new int[]{1, 2, 3, 4, 5};
         Heapsort.sort(input);
@@ -17,7 +17,7 @@ public class Heapsorttest {
     }
 
     @Test
-    public void test_2() {
+    public void check_negative_positive_num() {
         int[] input = new int[]{-10, 4, 7, 3, -2, 0};
         int[] answer = new int[]{-10, -2, 0, 3, 4, 7};
         Heapsort.sort(input);
@@ -25,23 +25,15 @@ public class Heapsorttest {
     }
 
     @Test
-    public void test_3() {
-        int[] input = new int[]{-1, -5, 0, 1, -2, -3};
-        int[] answer = new int[]{-5, -3, -2, -1, 0, 1};
+    public void check_empty_arr() {
+        int[] input = new int[]{};
+        int[] answer = new int[]{};
         Heapsort.sort(input);
         assertArrayEquals(answer, input);
     }
 
     @Test
-    public void test_4() {
-        int[] input = new int[]{-10, 150, 1, 6, -123, 32, 3};
-        int[] answer = new int[]{-123, -10, 1, 3, 6, 32, 150};
-        Heapsort.sort(input);
-        assertArrayEquals(answer, input);
-    }
-
-    @Test
-    public void test_5() {
+    public void check_max_min_int() {
         int[] input = new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE, 1, 6, -123, 32, 3};
         int[] answer = new int[]{Integer.MIN_VALUE, -123, 1, 3, 6, 32, Integer.MAX_VALUE};
         Heapsort.sort(input);
