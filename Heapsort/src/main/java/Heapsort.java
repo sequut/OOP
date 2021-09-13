9d4a069f2cc3d04aa246b4be722bf28440c72ab5
Andrew <andrewkostin255@ya.ru>
2021-09-13 21:06:36 +0700

    changed style

--- Heapsort/src/main/java/Heapsort.java
+++ Heapsort/src/main/java/Heapsort.java
@@ -24,8 +24,7 @@ public class Heapsort {
      * @param n length of array
      * @param i parent node
      */
-
-    static  void heapify(int[] aa, int n, int i) {
+    static void heapify(int[] aa, int n, int i) {
         int largest = i;
         int left = 2 * i + 1;
         int right = 2 * i + 2;
