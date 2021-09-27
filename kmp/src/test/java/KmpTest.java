23439a0de4455f84b1864e5ea15c21b52e1c34f9
Andrew <andrewkostin255@ya.ru>
2021-09-27 22:19:30 +0700

    corrected the numbering of tests

--- kmp/src/test/java/KmpTest.java
+++ kmp/src/test/java/KmpTest.java
@@ -11,7 +11,7 @@ public class KmpTest {
     @Test
     public void check_substring_with_substring() throws IOException {
         Kmp aa = new Kmp();
-        Integer[] check = aa.kmp("test2.txt", "aabaab");
+        Integer[] check = aa.kmp("test1.txt", "aabaab");
         Integer[] answer = new Integer[]{0, 10, 13, 16};
         assertArrayEquals(answer, check);
 
@@ -20,7 +20,7 @@ public class KmpTest {
     @Test
     public void check_empty_file() throws IOException {
         Kmp aa = new Kmp();
-        Integer[] check = aa.kmp("test4.txt", "test");
+        Integer[] check = aa.kmp("test2.txt", "test");
         Integer[] help = new Integer[]{};
         assertArrayEquals(help, check);
     }
@@ -28,7 +28,7 @@ public class KmpTest {
     @Test
     public void check_with_big_test() throws IOException {
         Kmp aa = new Kmp();
-        Integer[] check = aa.kmp("test6.txt", "abv");
+        Integer[] check = aa.kmp("test3.txt", "abv");
 
         Integer[] answer = new Integer[]{14400000};
         assertArrayEquals(answer, check);
