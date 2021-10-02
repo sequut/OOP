2c9365f28242368c72b2de9127e88648f651a37a
Andrew <andrewkostin255@ya.ru>
2021-10-03 03:08:10 +0700

    added test program and comments

--- stack/src/main/java/Stack.java
+++ stack/src/main/java/Stack.java
@@ -1,2 +1,81 @@
-public class Stack {
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Stack<Type> implements Iterable<Type> {
+
+    private int count = 0;
+    private ArrayList<Type> stack = new ArrayList<>();
+
+    /**
+     * returns number of elements in stack
+     * @return - returns how how many elements in stack right now
+     */
+    public int count(){
+        return count;
+    }
+
+    /**
+     * push one element type Type to stack
+     * @param aa - item
+     */
+    public void push(Type aa){
+        stack.add(aa);
+        count += 1;
+    }
+
+    /**
+     * push array of elements type Type to stack
+     * @param aa - array with items
+     */
+    public void pushStack(Type[] aa){
+        for (Type type : aa) {
+            stack.add(type);
+            count += 1;
+        }
+    }
+
+    /**
+     * delete one element from stack
+     */
+    public void pop(){
+        if (count > 0)
+            stack.remove(--count);
+    }
+
+    /**
+     * delete items from stack
+     * @param number - how many items we should delete
+     */
+    public void popStack(int number){
+        for (int i = 0; i < number; i++) {
+            stack.remove(count - 1);
+            count -= 1;
+        }
+    }
+
+    /**
+     * write current stack
+     */
+    public void write(){
+        for (int i = 0; i < count; i++)
+            System.out.println(stack.get(i));
+        System.out.println();
+    }
+
+    @Override
+    public Iterator<Type> iterator() {
+        return new Iterator<>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return count > index;
+            }
+
+            @Override
+            public Type next() {
+                return stack.get(index++);
+            }
+        };
+    }
 }
--- stack/src/test/java/StackTest.java
+++ stack/src/test/java/StackTest.java
@@ -1,4 +1,70 @@
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class StackTest {
 
+    @Test
+    public void testInt(){
+        Stack<Integer> test = new Stack<>();
 
+        test.push(1);
+        assertEquals(1, test.count());
+
+        test.push(345);
+        assertEquals(2, test.count());
+
+        Integer[] help = {6, 99, 100};
+        test.pushStack(help);
+        assertEquals(5, test.count());
+
+        test.write();
+
+        test.pop();
+        assertEquals(4, test.count());
+
+        test.popStack(3);
+        assertEquals(1, test.count());
+
+        test.write();
+
+        test.pop();
+        assertEquals(0, test.count());
+
+        test.pop();
+        assertEquals(0, test.count());
+    }
+
+    @Test
+    public void testStr(){
+        Stack<String> test = new Stack<>();
+
+        test.push("aa");
+        assertEquals(1, test.count());
+
+        test.push("bb");
+        assertEquals(2, test.count());
+
+        String[] help = {"cc", "dd", "ee"};
+        test.pushStack(help);
+        assertEquals(5, test.count());
+
+        test.write();
+
+        test.pop();
+        assertEquals(4, test.count());
+
+        test.popStack(3);
+        assertEquals(1, test.count());
+
+        test.write();
+
+        test.pop();
+        assertEquals(0, test.count());
+
+        test.pop();
+        assertEquals(0, test.count());
+    }
 }
