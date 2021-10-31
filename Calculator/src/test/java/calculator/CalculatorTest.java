1aae5410c476f8b7128653411743bc0ffdd42fdc
Andrew <andrewkostin255@ya.ru>
2021-10-31 23:26:20 +0700

    initial commit

---
+++ Calculator/src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,10 @@
+package calculator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
+public class CalculatorTest {
+}
