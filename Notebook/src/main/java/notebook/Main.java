dc45747eeb9afd1de7e85f44bec8e33b5a0a2c2e
Andrew <andrewkostin255@ya.ru>
2021-11-22 17:02:31 +0700

    added main

--- Notebook/src/main/java/notebook/Main.java
+++ Notebook/src/main/java/notebook/Main.java
@@ -1,41 +1,50 @@
 package notebook;
 
-import java.io.*;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.Date;
-import notebook.*;
 
-public class Main{
-    public static void main(String[] args){
-        if (args.length <= 0){
+public class Main {
+    public static void main(String[] args) throws ParseException {
+        if (args.length < 1) {
             System.out.println("no input parameters");
             return;
         }
-
-        //Notebook notebook = new Notebook("notes.json");
-        Notebook notebook = new Notebook("notes1.json");
-        //Notebook notebook = new Notebook();
-/*
-        switch (args[0]){
-            case "-add": {
-                break;
-            }
-            case "-show": {
-
-            }
-            case "": {
-
-            }
-            default: {
-                break;
-            }
-        }
-*/
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Notebook notebook = new Notebook(args[0] + ".json");
 
         for (String str: args)
             System.out.println(str);
 
+
+        switch (args[1]) {
+            case ("-add"): {
+                if (args.length > 3)
+                    notebook.add(new Note(args[2], args[3], new Date()));
+                else if (args.length == 3)
+                    notebook.add(new Note(args[2], "", new Date()));
+                else
+                    System.err.println("invalid input format");
+            }
+            case ("-rm"): {
+                if (args.length > 2)
+                    notebook.remove(args[2]);
+                else
+                    System.err.println("invalid input format");
+            }
+            case ("-show"): {
+                if (args.length == 2)
+                    notebook.show();
+                else if (args.length >= 5){
+                    String[] words = Arrays.copyOfRange(args, 4, args.length);
+                    notebook.show(dateFormat.parse(args[2]), dateFormat.parse(args[3]), words);
+                }
+                else
+                    System.err.println("invalid input format");
+            }
+            default:
+        }
     }
 }
