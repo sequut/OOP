43468959d25b6aafa3cbf1372a048a78bc972311
Andrew <andrewkostin255@ya.ru>
2022-02-22 01:50:24 +0700

    added data

--- PrimeNumbers/src/main/java/Main.java
+++ PrimeNumbers/src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args){
         createNumbers make = new createNumbers();
 
-        ArrayList<Integer> check = make.createRandom(1000000);
+        ArrayList<Integer> check = make.createRandom(100);
         wSeries series = new wSeries(check);
         long start = System.currentTimeMillis();
         System.out.println(series.count());
