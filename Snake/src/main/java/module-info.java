3686647f48efc7df1e95401ad66c38d7d2722717
Andrew <andrewkostin255@ya.ru>
2022-04-18 23:08:02 +0700

    almost final ssssnake

--- Snake/src/main/java/module-info.java
+++ Snake/src/main/java/module-info.java
@@ -2,7 +2,8 @@ module sequut.snake {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens sequut.gui to javafx.fxml;
     exports sequut.gui;
+    exports sequut.settings;
+    exports sequut.logic;
 }
